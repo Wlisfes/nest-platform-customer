cf0e912ad169d578957b0a5e824d86eca20d42df
妖雨纯 <876451336@qq.com>
2024-09-15 23:36:11 +0800

    🎉 init(Global): 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="tsx">
+import { defineComponent } from 'vue'
+import { RouterView } from 'vue-router'
+
+export default defineComponent({
+    name: 'App',
+    setup() {
+        return () => (
+            <layout-provider>
+                <layout-beforer>
+                    <RouterView></RouterView>
+                </layout-beforer>
+            </layout-provider>
+        )
+    }
+})
+</script>
